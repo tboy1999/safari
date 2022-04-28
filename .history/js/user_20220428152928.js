7e0fa8e48d25f46453fb71bbc2d28ea1f5792997
tongtai <meotai1999@gmail.com>
2022-04-28 18:20:01 +0700

    delete .history

--- .history/js/user_20220428152928.js
+++
@@ -1,5 +0,0 @@
-// user js----------------------------------------------------------------
-
-const userItems = document.querySelectAll('.user__item')
-
-console.log(userItems);
--- .history/js/user_20220428153028.js
+++
@@ -1,5 +0,0 @@
-// user js----------------------------------------------------------------
-
-const userItems = document.querySelectorAll('.user__item')
-
-console.log(userItems);
